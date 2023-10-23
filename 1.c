5e551836f462e3975dd77eb92abe41ef4ac74fb6
Tau0509 <1136036602@qq.com>
2023-10-23 18:55:12 +0800

    20231023

--- 1.c
+++ 1.c
@@ -1,7 +1,16 @@
 #include<stdio.h>
 int main(void){
-    int a=1;
-    a--? a++:a++;
-    printf("%d",a);
+    unsigned short x=1, y=2, z=4, mask=0xc3, w;
+	short v;
+    printf("%d ",(~x&x));
+    printf("%d ",(v=~x));
+    printf("%d ",(~x^x));
+    printf("%d ",(x|y&x|z));
+       printf("%d ",(w=y|z, (w<<3)+(w<<1)));
+    printf("%d ",(x|(y&x)|z<<y^mask>>x));
+    printf("%d ",(-1, v<<=1));
+    printf("%d ",(~x|x));
+        printf("%d ",(x^~y));
+    printf("%d ",(x|y|z>>2));
     return 0;
 }
